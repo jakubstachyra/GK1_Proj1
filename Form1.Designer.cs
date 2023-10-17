188e79455e12fad6a007491b862d5da06394ed9b
Jakub Stachyra <103006161+jakubstachyra@users.noreply.github.com>
2023-10-18 00:12:24 +0200

    Moving vertexes, and adding vertex in the center of edge (#6)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,15 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.bitMap = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.bitMap)).BeginInit();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bitMap = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bitMap)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(615, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 350);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 88);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Algorithm";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(147, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Brezenham\'s algorithm";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(116, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Library algorithm";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 6);
+            this.groupBox2.MinimumSize = new System.Drawing.Size(176, 338);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(176, 338);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mode";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 304);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Offset";
             // 
             // bitMap
             // 
@@ -45,56 +116,14 @@
             this.bitMap.Location = new System.Drawing.Point(0, 0);
             this.bitMap.Name = "bitMap";
             this.bitMap.Size = new System.Drawing.Size(800, 450);
-            this.bitMap.TabIndex = 0;
+            this.bitMap.TabIndex = 2;
             this.bitMap.TabStop = false;
             this.bitMap.Paint += new System.Windows.Forms.PaintEventHandler(this.bitMap_Paint);
             this.bitMap.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bitMap_MouseClick);
+            this.bitMap.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.bitMap_MouseDoubleClick);
+            this.bitMap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.bitMap_MouseDown);
             this.bitMap.MouseMove += new System.Windows.Forms.MouseEventHandler(this.bitMap_MouseMove);
-            // 
-            // panel1
-            // 
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(634, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(166, 450);
-            this.panel1.TabIndex = 1;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.AutoSize = true;
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.groupBox1.Location = new System.Drawing.Point(-3, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(169, 450);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Algorithm";
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(16, 47);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(116, 19);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Library algorithm";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(16, 84);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(147, 19);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.Text = "Brezenham\'s algorithm";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.bitMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bitMap_MouseUp);
             // 
             // Form1
             // 
@@ -105,21 +134,24 @@
             this.Controls.Add(this.bitMap);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.bitMap)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bitMap)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private PictureBox bitMap;
         private Panel panel1;
         private GroupBox groupBox1;
         private RadioButton radioButton2;
         private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private TextBox textBox1;
+        private PictureBox bitMap;
     }
 }
---
+++ Functions.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GK_Proj1
+{
+    static class Functions
+    {
+        public static void BresenhamLine(UserControl usercontrol  , Point P1, Point P2, Color color, Bitmap canvas)
+        {
+            usercontrol.BackgroundImage = null;
+            int dx = P2.X - P1.X;
+            int dy = P2.Y - P1.Y;
+            Point d1 = new Point(Math.Sign(dx), Math.Sign(dy));
+
+            Point d2;
+            int longerDim = Math.Abs(dx);
+            int shorterDim = Math.Abs(dy);
+            if (longerDim < shorterDim)
+            {
+                (longerDim, shorterDim) = (shorterDim, longerDim);
+                d2 = new Point(0, d1.Y);
+            }
+            else
+                d2 = new Point(d1.X, 0);
+            
+            int numerator = longerDim >> 1;
+
+            for (int i = 0; i <= longerDim; ++i)
+            {
+                if (P1.X > 0 && P1.Y > 0 && P1.X < canvas.Width && P1.Y < canvas.Height)
+                    canvas.SetPixel(P1.X, P1.Y, color);
+
+                numerator += shorterDim;
+                if (numerator >= longerDim)
+                {
+                    numerator -= longerDim;
+                    P1.Offset(d1);
+                }
+                else
+                {
+                    P1.Offset(d2);
+                }
+            }
+            usercontrol.BackgroundImage = canvas;
+
+        }
+        public static void DrawPolygon(PaintEventArgs e, List<Polygon> polygons, int vertexSize, int offset)
+        {
+            
+            foreach (var polygon in polygons)
+            {
+                foreach (var vertex in polygon.vertices)
+                {
+                    e.Graphics.FillEllipse(Brushes.Black,  vertex.point.X - offset, vertex.point.Y - offset, vertexSize ,vertexSize);
+                    if (vertex.next != null)
+                    {
+                        e.Graphics.DrawLine(Pens.Black, vertex.point, vertex.next.point);
+                    }
+                }
+            }
+
+        }
+        public static bool isPointOnLine(Point P1, Point P2, Point P, int tolerance)
+        {
+            double distanceP1ToP2 = Math.Sqrt(Math.Pow(P2.X - P1.X, 2) + Math.Pow(P2.Y - P1.Y, 2));
+
+            // Oblicz długość odcinka między P1 a P.
+            double distanceP1ToP = Math.Sqrt(Math.Pow(P.X - P1.X, 2) + Math.Pow(P.Y - P1.Y, 2));
+
+            // Oblicz długość odcinka między P2 a P.
+            double distanceP2ToP = Math.Sqrt(Math.Pow(P2.X - P.X, 2) + Math.Pow(P2.Y - P.Y, 2));
+
+            // Oblicz odległość punktu P od prostej między P1 i P2.
+            double d = Math.Abs((P2.X - P1.X) * (P1.Y - P.Y) - (P1.X - P.X) * (P2.Y - P1.Y)) /
+                       distanceP1ToP2;
+
+            if (d <= tolerance && distanceP1ToP + distanceP2ToP <= distanceP1ToP2 + tolerance)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public static Point FindCenterOfLine(Point P1, Point P2)
+        {
+            return new Point((P1.X + P2.X) / 2, (P1.Y + P2.Y) / 2);
+        }
+        public static double CalculateDistance(Point point1, Point point2)
+        {
+            int dx = point2.X - point1.X;
+            int dy = point2.Y - point1.Y;
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        //public static Point GetCloserPoint(Point P1, Point P2, Point P)
+        //{
+        //    double distanceTo1 = CalculateDistance(P1, P);
+        //    double distanceTo2 = CalculateDistance(P, P2);
+
+        //    if (distanceTo1 < distanceTo2)
+        //    {
+        //        return P1;
+        //    }
+        //    else
+        //    {
+        //        return P2;
+        //    }
+        //}
+       
+
+    }
+}
